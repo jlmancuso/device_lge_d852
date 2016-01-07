e3f56ec5b8948a8f7c9e2f99f13d356306e74d0e
roguesyko <roguesyko@gmail.com>
2016-01-06 22:05:29 -0500

    1 more broken piece

---
+++ broken.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/lge/d852/full_d852.mk)
+
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := broken_d852
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_bell_ca" \
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
