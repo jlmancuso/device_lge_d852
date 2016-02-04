6fd44bfad5015eaa85c3a60ff1b0f04c3993220b
roguesyko <roguesyko@gmail.com>
2016-02-04 12:10:37 -0500

    Add device maintainer and update build fingerprint from d855

--- broken.mk
+++ broken.mk
@@ -12,3 +12,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_bell_ca" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 15351124649f4 release-keys"
+    
+# Broken Device Maintainers
+    PRODUCT_BUILD_PROP_OVERRIDES += \
+ 	DEVICE_MAINTAINERS="Joe (roguesyko) Repasy" 
